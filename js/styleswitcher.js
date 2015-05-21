311992862d82809d54653029ce9c8f761fcc15eb
Thomas Lohninger <thomas@lohninger.org>
2015-05-22 01:59:56 +0200

    alpha progressbar rewrite

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,13 +1,23 @@
 var progress = 0;
 
-function set_progress( new_percent_value )
-{
-	var e = document.getElementById('progress');
-	var p = Math.floor(progress * 10) / 10;
+var steps = [50, 100, 250, 500, 750, 1000, 1500, 2500, 4000, 5000, 7500, 10000, 12500, 15000, 20000, 25000, 30000, 50000, 100000, 250000, 8000000];
+function getTarget(cnt) {
+	for (var e in steps) {
+		if (cnt < steps[e]) {
+			return steps[e];
+		}
+	}
+}
 
-	e.style.width = progress + '%';
-	e.innerHTML = 'Fortschritt: ' + p + '%';
-}  // set_progress
+function set_progress( signatures )
+{
+	signatures = signatures || 0;
+	var target = getTarget(signatures);
+	var percent = Math.floor(signatures/target)*100 + '%';
+
+	$('#progress').animate({'width': percent}, 1500).html('Fortschritt:' + percent);
+	$('progress_description').html(signatures + ' Unterschriften von ' + target);
+}
 
 
 function AddEvent( element, event_name, event_function )
