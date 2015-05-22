aa85541b41c709ff41cd81277cd9a982766e950e
Thomas Lohninger <thomas@lohninger.org>
2015-05-22 02:43:49 +0200

    cleanup

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,5 +1,3 @@
-var progress = 0;
-
 var steps = [50, 100, 250, 500, 750, 1000, 1500, 2500, 4000, 5000, 7500, 10000, 12500, 15000, 20000, 25000, 30000, 50000, 100000, 250000, 8000000];
 function getTarget(cnt) {
 	for (var e in steps) {
@@ -19,40 +17,9 @@ function set_progress( signatures )
 	$('progress_description').html(signatures + ' Unterschriften von ' + target);
 }
 
+$(function () {
+	set_progress(cnt.cnt);
+});
 
-function AddEvent( element, event_name, event_function )
-{
-	if (element.attachEvent) {             // Internet Explorer
-		element.attachEvent( "on" + event_name, function() {event_function.call(element);} );
-	}
-	else if (element.addEventListener) {   // Rest of the world
-		element.addEventListener(event_name, event_function, false);
-	}
-} // AddEvent
-
-
-// MAIN PROGRAM //
-
-
-AddEvent( window, 'load', function () {
-	/*
-	var interval = window.setInterval( function () {
-		if (progress < 100) {
-			progress += 5;
-			set_progress( progress );
-		} else {
-			window.clearInterval( interval );
-		}
-	}, 200 );
-	*/
-	max = 100;
-	if (typeof cnt !== 'undefined') {
-		set_progress(Math.min(cnt.cnt/max, 100));
-	}
-	else {
-		// set safe limit here
-		//set_progress();	
-	}
-}); // onLoad
 
 
