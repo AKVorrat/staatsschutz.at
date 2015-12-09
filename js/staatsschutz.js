21985b2507a796d76ac390f5ef31894814345114
Thomas Lohninger <thomas@lohninger.org>
2015-12-09 19:39:23 +0100

    twitter tempalte text change

--- js/staatsschutz.js
+++ js/staatsschutz.js
@@ -37,8 +37,8 @@ $(function () {
 
 // countdown 
 var _translate_twords = {
-  'en': {'hs': 'hours', 'ds': 'days', 'ms': 'minutes', 'ss': 'seconds', 'h': 'hour', 'd': 'day', 'm': 'minute', 's': 'second', 'suffix': 'until the vote', 'prefix': 'New secret agency in', 'smprefix': 'Austria establishes 10 new secret agencies in ', 'smsuffix': '.\nprevent #Staatsschutz!\nhttps://www.staatsschutz.at/en'}
-  , 'de': {'hs': 'Stunden', 'ds': 'Tagen', 'ms': 'Minuten', 'ss': 'Sekunden', 'h': 'Stunde', 'd': 'Tag', 'm': 'Minute', 's': 'Sekunde', 'suffix': 'bis zur Abstimmung', 'prefix': 'Staatsschutz kommt in', 'smprefix': 'Österreich bekommt zehn neue Geheimdienste in ', 'smsuffix': '.\n#Staatsschutz verhindern!\nhttps://www.staatsschutz.at'}
+  'en': {'hs': 'hours', 'ds': 'days', 'ms': 'minutes', 'ss': 'seconds', 'h': 'hour', 'd': 'day', 'm': 'minute', 's': 'second', 'suffix': 'until the vote', 'prefix': 'New secret agency in', 'smprefix': 'Austria establishes a new intelligence agency in ', 'smsuffix': '.\nprevent #Staatsschutz!\nhttps://www.staatsschutz.at/en'}
+  , 'de': {'hs': 'Stunden', 'ds': 'Tagen', 'ms': 'Minuten', 'ss': 'Sekunden', 'h': 'Stunde', 'd': 'Tag', 'm': 'Minute', 's': 'Sekunde', 'suffix': 'bis zur Abstimmung', 'prefix': 'Staatsschutz kommt in', 'smprefix': 'Österreich bekommt einen neuen Inlandsgeheimdienst in ', 'smsuffix': '.\n#Staatsschutz verhindern!\nhttps://www.staatsschutz.at'}
   , 'fr': {'hs': 'heures', 'ds': 'jours', 'ms': 'minutes', 'ss': 'secondes', 'h': 'heure', 'd': 'jour', 'm': 'minute', 's': 'seconde', 'suffix': 'jusqu\'au vote', 'prefix': 'Vote dans', 'smprefix': 'Plus que ', 'smsuffix': ' pour sauver internet. Agissez, rendez-vous sur http://savetheinternet.eu/fr #netneutralité'}
   , 'es': {'hs': 'horas', 'ds': 'días', 'ms': 'minutos', 'ss': 'segundos', 'h': 'hora', 'd': 'día', 'm': 'minuto', 's': 'segundo', 'suffix': 'hasta el voto', 'prefix': 'Voto en', 'smprefix': 'Quedan ', 'smsuffix': ' para salvar internet. Actúa ahora en http://savetheinternet.eu/es #neutralidaddelared'}
 };
