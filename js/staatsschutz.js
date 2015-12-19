4af454fed2f1d6c9d813d5e4f2c54a9f72ca47a2
PublicScam <publicscam@gmx.at>
2015-12-19 17:44:30 +0100

    fix header, fix css split

--- js/staatsschutz.js
+++ js/staatsschutz.js
@@ -39,10 +39,10 @@ $(function () {
 
 // countdown 
 var _translate_twords = {
-  'en': {'hs': 'hours', 'ds': 'days', 'ms': 'minutes', 'ss': 'seconds', 'h': 'hour', 'd': 'day', 'm': 'minute', 's': 'second', 'suffix': 'until the vote', 'prefix': 'New secret agency in', 'smprefix': 'Austria establishes a new intelligence agency in ', 'smsuffix': '.\nprevent #Staatsschutz!\nhttps://www.staatsschutz.at/en'}
-  , 'de': {'hs': 'Stunden', 'ds': 'Tagen', 'ms': 'Minuten', 'ss': 'Sekunden', 'h': 'Stunde', 'd': 'Tag', 'm': 'Minute', 's': 'Sekunde', 'suffix': 'bis zur Abstimmung', 'prefix': 'Staatsschutz kommt in', 'smprefix': 'Österreich bekommt einen neuen Inlandsgeheimdienst in ', 'smsuffix': '.\n#Staatsschutz verhindern!\nhttps://www.staatsschutz.at'}
-  , 'fr': {'hs': 'heures', 'ds': 'jours', 'ms': 'minutes', 'ss': 'secondes', 'h': 'heure', 'd': 'jour', 'm': 'minute', 's': 'seconde', 'suffix': 'jusqu\'au vote', 'prefix': 'Vote dans', 'smprefix': 'Plus que ', 'smsuffix': ' pour sauver internet. Agissez, rendez-vous sur http://savetheinternet.eu/fr #netneutralité'}
-  , 'es': {'hs': 'horas', 'ds': 'días', 'ms': 'minutos', 'ss': 'segundos', 'h': 'hora', 'd': 'día', 'm': 'minuto', 's': 'segundo', 'suffix': 'hasta el voto', 'prefix': 'Voto en', 'smprefix': 'Quedan ', 'smsuffix': ' para salvar internet. Actúa ahora en http://savetheinternet.eu/es #neutralidaddelared'}
+  'en': {'hs': 'hours', 'ds': 'days', 'ms': 'minutes', 'ss': 'seconds', 'h': 'hour', 'd': 'day', 'm': 'minute', 's': 'second', 'suffix': 'until the vote', 'prefix': 'New secret agency in', 'smprefix': 'Austria establishes a new intelligence agency in\<br \/\>', 'smsuffix': '.\nprevent #Staatsschutz!\nhttps://www.staatsschutz.at/en'}
+  , 'de': {'hs': 'Stunden', 'ds': 'Tagen', 'ms': 'Minuten', 'ss': 'Sekunden', 'h': 'Stunde', 'd': 'Tag', 'm': 'Minute', 's': 'Sekunde', 'suffix': 'bis zur Abstimmung', 'prefix': 'Staatsschutz kommt in\<br \/\>', 'smprefix': 'Österreich bekommt einen neuen Inlandsgeheimdienst in ', 'smsuffix': '.\n#Staatsschutz verhindern!\nhttps://www.staatsschutz.at'}
+  , 'fr': {'hs': 'heures', 'ds': 'jours', 'ms': 'minutes', 'ss': 'secondes', 'h': 'heure', 'd': 'jour', 'm': 'minute', 's': 'seconde', 'suffix': 'jusqu\'au vote', 'prefix': 'Vote dans', 'smprefix': 'Plus que ', 'smsuffix': ' pour sauver internet. Agissez, rendez-vous sur https://www.staatsschutz.at'}
+  , 'es': {'hs': 'horas', 'ds': 'días', 'ms': 'minutos', 'ss': 'segundos', 'h': 'hora', 'd': 'día', 'm': 'minuto', 's': 'segundo', 'suffix': 'hasta el voto', 'prefix': 'Voto en', 'smprefix': 'Quedan ', 'smsuffix': ' para salvar internet. Actúa ahora en https://www.staatsschutz.at'}
 };
 
 var plenary_vote = new Date(2016, 0, 19, 13, 0);  // innenausschuss
